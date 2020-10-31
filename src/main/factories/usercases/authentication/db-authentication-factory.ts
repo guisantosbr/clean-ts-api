6396462e01811f0e0378529cc7a6899fef112c1f
Guilherme Almeida <guilherme.almeida@pebmed.com.br>
2020-10-30 22:01:27 -0300

    refactor: separete factor in folders

---
+++ src/main/factories/usercases/authentication/db-authentication-factory.ts
@@ -0,0 +1,15 @@
+
+import { AccountMongoRepository } from '../../../../infra/db/mongodb/account/account-mongo-repository'
+import { DbAuthentication } from '../../../../data/usecases/authentication/db-authentication'
+import { BcryptAdapter } from '../../../../infra/criptography/bcrypt-adapter/bcrypt-adapter'
+import { JwtAdapter } from '../../../../infra/criptography/jwt-adapter/jwt-adapter'
+import env from '../../../config/env'
+import { Authentication } from '../../../../domain/usercases/authentication'
+
+export const makeDbAuthentication = (): Authentication => {
+  const salt = 12
+  const bcryptAdapter = new BcryptAdapter(salt)
+  const jwtAdapter = new JwtAdapter(env.jwtSecret)
+  const accountMongoRepository = new AccountMongoRepository()
+  return new DbAuthentication(accountMongoRepository, bcryptAdapter, jwtAdapter, accountMongoRepository)
+}
