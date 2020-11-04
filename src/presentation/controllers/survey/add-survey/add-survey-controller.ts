81ff16c28d1409b14ed773691adb939f5fb32927
Guilherme Almeida <guilherme.almeida@pebmed.com.br>
2020-11-04 20:16:26 -0300

    feat: ensure AddSurveyController calls AddSurveyUseCase with correct values

--- src/presentation/controllers/survey/add-survey/add-survey-controller.ts
+++ src/presentation/controllers/survey/add-survey/add-survey-controller.ts
@@ -1,9 +1,10 @@
-import { Controller, HttpRequest, HttpResponse, Validation } from './add-survery-controller-protocols'
+import { AddSurvey, Controller, HttpRequest, HttpResponse, Validation } from './add-survery-controller-protocols'
 import { badRequest } from '../../../helpers/http/http-helper'
 
 export class AddSurveyController implements Controller {
   constructor (
-    private readonly validation: Validation
+    private readonly validation: Validation,
+    private readonly addSurvey: AddSurvey
   ) {
   }
 
@@ -12,6 +13,8 @@ export class AddSurveyController implements Controller {
     if (error) {
       return badRequest(error)
     }
-    return Promise.resolve(null)
+    const { question, answers } = httpRequest.body
+    await this.addSurvey.add({ question, answers })
+    return null
   }
 }
