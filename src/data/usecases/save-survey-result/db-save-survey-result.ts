c0d1ce6621b3eed5b00a9494a7aee9099b0b47b4
Guilherme Almeida <guilherme.almeida@pebmed.com.br>
2020-11-16 23:13:52 -0300

    feat: ensure DbSaveSurveyResult returns a SurveyResult on success

--- src/data/usecases/save-survey-result/db-save-survey-result.ts
+++ src/data/usecases/save-survey-result/db-save-survey-result.ts
@@ -5,7 +5,6 @@ export class DbSaveSurveyResult implements SaveSurveyResult {
   }
 
   async save (data: SaveSurveyResultModel): Promise<SurveyResultModel> {
-    await this.saveSurveyResultRepository.save(data)
-    return Promise.resolve(undefined)
+    return this.saveSurveyResultRepository.save(data)
   }
 }
